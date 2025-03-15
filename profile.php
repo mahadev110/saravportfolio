4e7cb5da430228df0b6248c77a0ec24caf1ee2f8
Mahadevan <magadev110@gmail.com>
2025-03-16 02:23:18 +0530

    profile.php update

--- profile.php
+++ profile.php
@@ -10,28 +10,28 @@ include "includes/navbar.php";
         <div class="row">
             <div class="col-lg-6">
                 <div class="categories_post">
-                    <img src="img/banner/bannerleft2.jpg" alt="post">
+                    <img src="img/projects/proj_orion.jpg" alt="post">
                     <div class="categories_details">
                         <div class="categories_text">
-                            <a href="blog-details.html">
-                                <h5>Social Life</h5>
-                            </a>
+                           
+                                <h5>ORION</h5>
+                           
                             <div class="border_line"></div>
-                            <p>Enjoy your social life together</p>
+                            <p>Innovate, Elevate, Dominate.</p>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="categories_post">
-                    <img src="img/banner/bannerleft2.jpg" alt="post">
+                    <img src="img/projects/orion-slide1.jpg" alt="post">
                     <div class="categories_details">
                         <div class="categories_text">
                             <a href="blog-details.html">
-                                <h5>Politics</h5>
+                                <h5>Claire Studio</h5>
                             </a>
                             <div class="border_line"></div>
-                            <p>Be a part of politics</p>
+                            <p>Where Creativity Meets Precision.</p>
                         </div>
                     </div>
                 </div>
@@ -65,7 +65,7 @@ include "includes/navbar.php";
                             <li class="nav-item">
                                 <a class="nav-link" id="pills-description-tab" data-toggle="pill"
                                     href="#tab-description" role="tab" aria-controls="pills-description"
-                                    aria-selected="false">Description</a>
+                                    aria-selected="false">Technical Details</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" id="pills-advantage-tab" data-toggle="pill" href="#tab-advantage"
@@ -75,18 +75,213 @@ include "includes/navbar.php";
                         <div class="tab-content" id="pills-tabContent">
                             <div class="tab-pane fade show active" id="tab-feature" role="tabpanel"
                                 aria-labelledby="pills-home-tab">
-                                <div class="main_title">
-        			<h2>Latest Posts from Blog</h2>
-        			<p>If you are looking at blank cassettes on the web, you may be very confused at the difference in price. You may see some for as low as $.17 each.</p>
-        		</div>
+                                <section class="portfolio_details_area mt-5">
+                                    <div class="container">
+                                        <div class="portfolio_details_inner">
+
+                                            <h4>Modern & Minimalist Design</h4>
+                                            <ul>
+                                                <li>Sleek and sophisticated UI with a focus on aesthetics.</li>
+                                                <li>A well-structured layout that enhances user engagement.</li>
+                                                <li>Intuitive navigation for a seamless user experience.</li>
+                                            </ul>
+
+                                            <h4>High Performance & Scalability</h4>
+                                            <ul>
+                                                <li>Optimized for speed and efficiency, ensuring fast load times.</li>
+                                                <li>Scalable architecture to support growing demands.</li>
+                                                <li>Cloud-based deployment for flexibility and reliability.</li>
+                                            </ul>
+
+                                            <h4>Advanced Security Mechanisms</h4>
+                                            <ul>
+                                                <li>End-to-end encryption for data protection.</li>
+                                                <li>Multi-layer authentication for enhanced security.</li>
+                                                <li>Regular updates to mitigate potential vulnerabilities.</li>
+                                            </ul>
+
+                                            <h4>Seamless Integration Capabilities</h4>
+                                            <ul>
+                                                <li>API support for third-party tool integrations.</li>
+                                                <li>Compatible with multiple platforms and devices.</li>
+                                                <li>Real-time synchronization for enhanced workflow.</li>
+                                            </ul>
+
+                                            <h4>User-Centric Functionality</h4>
+                                            <ul>
+                                                <li>Personalized dashboard with real-time analytics.</li>
+                                                <li>AI-powered recommendations for an optimized experience.</li>
+                                                <li>Multi-language support for global accessibility.</li>
+                                            </ul>
+
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                                commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+                                            <p>Voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                                                sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                                                mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus
+                                                error sit voluptatem accusantium doloremque laudantium, totam rem
+                                                aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
+                                                beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+                                                voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
+                                                dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+                                                est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+                                                sed quia non numquam eius modi tempora incidunt ut labore et dolore
+                                                magnam aliquam quaerat voluptatem.</p>
+                                        </div>
+                                    </div>
+                                </section>
                             </div>
+                            <!-- tab 2  -->
                             <div class="tab-pane fade" id="tab-description" role="tabpanel"
                                 aria-labelledby="pills-profile-tab">
-2
+                                <section class="feature_area mt-5" id="technicaldetails">
+                                    <div class="container">
+                                        <div class="main_title">
+
+                                            <p style="padding-top: 30px;">
+                                                A robust and scalable technology stack powering the Orion Project with
+                                                cutting-edge innovations.
+                                            </p>
+                                        </div>
+                                        <div class="feature_inner row">
+                                            <div class="col-lg-4 col-md-6">
+                                                <div class="feature_item">
+                                                    <h4>Technology Stack</h4>
+                                                    <p>Built with modern frameworks like React.js, Node.js, and Python
+                                                        for seamless performance.</p>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-4 col-md-6">
+                                                <div class="feature_item">
+                                                    <h4>Cloud & Hosting</h4>
+                                                    <p>Deployed on AWS and Azure for high availability, security, and
+                                                        scalability.</p>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-4 col-md-6">
+                                                <div class="feature_item">
+                                                    <h4>Database Management</h4>
+                                                    <p>Uses PostgreSQL and MongoDB for structured and unstructured data
+                                                        handling.</p>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-4 col-md-6">
+                                                <div class="feature_item">
+                                                    <h4>Security Measures</h4>
+                                                    <p>Implements OAuth 2.0 authentication, SSL encryption, and
+                                                        automated security patches.</p>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-4 col-md-6">
+                                                <div class="feature_item">
+                                                    <h4>Integration Support</h4>
+                                                    <p>Offers RESTful APIs and GraphQL support for seamless third-party
+                                                        integrations.</p>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-4 col-md-6">
+                                                <div class="feature_item">
+                                                    <h4>Performance Optimization</h4>
+                                                    <p>Leverages caching strategies and CDNs for faster load times and
+                                                        minimal downtime.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>
+
                             </div>
+                            <!-- tabs 3  -->
                             <div class="tab-pane fade" id="tab-advantage" role="tabpanel"
                                 aria-labelledby="pills-profile-tab">
-3
+                                <section class="latest_blog_area mt-5" id="advantageid">
+                                    <div class="container">
+                                        <div class="main_title">
+
+                                            <p>
+                                                Unlocking innovation, efficiency, and seamless integration with
+                                                cutting-edge technology.
+                                            </p>
+                                        </div>
+                                        <div class="row latest_blog_inner">
+                                            <div class="col-lg-3">
+                                                <div class="l_blog_item">
+                                                    <div class="l_blog_text">
+                                                        <div class="date">
+                                                            <a href="#">Modern Design</a>
+                                                        </div>
+                                                        <a href="#">
+                                                            <h4>Sleek & Minimalist UI</h4>
+                                                        </a>
+                                                        <p>
+                                                            A visually appealing, user-friendly interface with a
+                                                            well-structured layout to enhance engagement.
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-3">
+                                                <div class="l_blog_item">
+                                                    <div class="l_blog_text">
+                                                        <div class="date">
+                                                            <a href="#">High Performance</a>
+                                                        </div>
+                                                        <a href="#">
+                                                            <h4>Optimized Speed & Scalability</h4>
+                                                        </a>
+                                                        <p>
+                                                            Designed for efficiency, ensuring fast load times,
+                                                            scalability, and cloud-based deployment.
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-3">
+                                                <div class="l_blog_item">
+                                                    <div class="l_blog_text">
+                                                        <div class="date">
+                                                            <a href="#">Advanced Security</a>
+                                                        </div>
+                                                        <a href="#">
+                                                            <h4>Robust Security Framework</h4>
+                                                        </a>
+                                                        <p>
+                                                            Features end-to-end encryption, multi-layer authentication,
+                                                            and regular security updates.
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-3">
+                                                <div class="l_blog_item">
+                                                    <div class="l_blog_text">
+                                                        <div class="date">
+                                                            <a href="#">Seamless Integration</a>
+                                                        </div>
+                                                        <a href="#">
+                                                            <h4>API & Multi-Platform Support</h4>
+                                                        </a>
+                                                        <p>
+                                                            Supports third-party API integrations, real-time
+                                                            synchronization, and cross-platform compatibility.
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>
+
                             </div>
 
                         </div>
@@ -98,7 +293,7 @@ include "includes/navbar.php";
                     <h2 class="mb-0">
                         <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
                             data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                            ML FLOW
+                            Claire Studio
                         </button>
                     </h2>
                 </div>
